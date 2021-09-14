2d3a5099e293782225b91a6d5bc3015c27a8743b
Ricardo Medina <71156336+Skydoes10@users.noreply.github.com>
2021-09-14 18:13:14 -0500

    Serialization methods added

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -1,9 +1,20 @@
 package model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Restaurant {
+	public String EMPLOYEES_FILE_NAME = "data/employees.emp";
+	public String INGREDIENTS_FILE_NAME = "data/ingredients.ing";
+	public String DISHES_FILE_NAME = "data/dishes.dsh";
+	public String ORDERS_FILE_NAME = "data/orders.ord";
 	private List<Employee> employees;
 	private List<Ingredient> ingredients;
 	private List<Dish> dishes;
@@ -17,16 +28,18 @@ public class Restaurant {
 	}
 	
 	// Employee
-	public void addEmployee(String name, String numID, String bDay, String pw) {
+	public void addEmployee(String name, String numID, String bDay, String pw) throws FileNotFoundException, IOException {
 		Employee employee = new Employee(name, numID, bDay, pw);
 		employees.add(employee);
+		saveEmployees();
 	}
 	
-	private void deleteEmployee(Employee e) {
+	private void deleteEmployee(Employee e) throws FileNotFoundException, IOException {
 		employees.remove(e);
+		saveEmployees();
 	}
 	
-	public void findEmployee(String numID) {
+	public void findEmployee(String numID) throws FileNotFoundException, IOException {
 		for(int i=0; i<employees.size(); i++) {
 			if(employees.get(i).getNumID().equals(numID)) {
 				deleteEmployee(employees.get(i));
@@ -38,17 +51,19 @@ public class Restaurant {
 		
 	}
 	
-	// Ingredients
-	public void addIngredient(String name, boolean amount, String unit) {
+	// Ingredient
+	public void addIngredient(String name, boolean amount, String unit) throws FileNotFoundException, IOException {
 		Ingredient ingredient = new Ingredient(name, amount, unit);
 		ingredients.add(ingredient);
+		saveIngredient();
 	}
 	
-	private void deleteIngredient(Ingredient i) {
+	private void deleteIngredient(Ingredient i) throws FileNotFoundException, IOException {
 		ingredients.remove(i);
+		saveIngredient();
 	}
 	
-	public void findIngredient(String name) {
+	public void findIngredient(String name) throws FileNotFoundException, IOException {
 		for(int i=0; i<ingredients.size(); i++) {
 			if(ingredients.get(i).getName().equals(name)) {
 				deleteIngredient(ingredients.get(i));
@@ -56,5 +71,118 @@ public class Restaurant {
 		}
 	}
 	
+	public void changeAmount(boolean newAmount) {
+		
+	}
+	
+	// Dish
+	public void addDish(String name, int price) throws FileNotFoundException, IOException {
+		Dish dish = new Dish(name, price);
+		dishes.add(dish);
+		saveDish();
+	}
+	
+	private void deleteDish(Dish d) throws FileNotFoundException, IOException {
+		dishes.remove(d);
+		saveDish();
+	}
+	
+	public void findDish(String name) throws FileNotFoundException, IOException {
+		for(int i=0; i<dishes.size(); i++) {
+			if(dishes.get(i).getName().equals(name)) {
+				deleteDish(dishes.get(i));
+			}
+		}
+	}
+	
+	// Order
+	public void addOrder(String code, String status, String date) throws FileNotFoundException, IOException {
+		Order order = new Order(code, status, date);
+		orders.add(order);
+		saveOrder();
+	}
+	
+	public void deleteOrder(Order o) throws FileNotFoundException, IOException {
+		orders.remove(o);
+		saveOrder();
+	}
+	
+	public boolean findOrder(String code) {
+		boolean found = false;
+		for(int i=0; i<orders.size(); i++) {
+			if(orders.get(i).getCode().equals(code)) {
+				found = true;
+			}
+		}
+		return found;
+	}
+	
+	public void changeStatus(String code, String newStatus) {
+		
+	}
+	
+	// Serialization Employee
+	private void saveEmployees() throws FileNotFoundException, IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(EMPLOYEES_FILE_NAME));
+		oos.writeObject(employees);
+		oos.close();
+	}
+	
+	private void loadEmployees() throws IOException, ClassNotFoundException {
+		File f = new File(EMPLOYEES_FILE_NAME);
+		if(f.exists()) {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+			employees = (ArrayList<Employee>) ois.readObject();
+			ois.close();
+		}
+	}
+	
+	// Serialization Ingredient
+	private void saveIngredient() throws FileNotFoundException, IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(INGREDIENTS_FILE_NAME));
+		oos.writeObject(ingredients);
+		oos.close();
+	}
+	
+	private void loadIngredient() throws IOException, ClassNotFoundException {
+		File f = new File(INGREDIENTS_FILE_NAME);
+		if(f.exists()) {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+			ingredients = (ArrayList<Ingredient>) ois.readObject();
+			ois.close();
+		}
+	}
+	
+	// Serialization Dish
+	private void saveDish() throws FileNotFoundException, IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(DISHES_FILE_NAME));
+		oos.writeObject(dishes);
+		oos.close();
+	}
+	
+	private void loadDish() throws IOException, ClassNotFoundException {
+		File f = new File(DISHES_FILE_NAME);
+		if(f.exists()) {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+			dishes = (ArrayList<Dish>) ois.readObject();
+			ois.close();
+		}
+	}
+	
+	// Serialization Order
+	private void saveOrder() throws FileNotFoundException, IOException {
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(ORDERS_FILE_NAME));
+		oos.writeObject(orders);
+		oos.close();
+	}
+	
+	private void loadOrder() throws IOException, ClassNotFoundException {
+		File f = new File(ORDERS_FILE_NAME);
+		if(f.exists()) {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(f));
+			orders = (ArrayList<Order>) ois.readObject();
+			ois.close();
+		}
+	}
 	
 }
